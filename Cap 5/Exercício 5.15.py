921e574e76ee013aea1f455707361037d7b79f11
Caue Vieira <cauevieira_@hotmail.com>
2018-04-07 08:46:55 -0300

    Learning.

--- Cap 5/Exercício 5.15.py
+++ Cap 5/Exercício 5.15.py
@@ -3,30 +3,19 @@ quantidade = 0
 custo = 0
 acumulado = 0
 soma = 0
-preco = 0
 
 
+preco = {1: 0.50, 2: 1, 3:4, 5:7, 9:9}
 
 while True:
-    def calculadora(preco):
-        quantidade = int(input("Digite a quantidade comprada: "))
-        custo = quantidade * preco
-        soma = 0
-        soma = soma + custo
     codigo = int(input("Digite o codigo do produto: "))
     if codigo == 0:
         break
-    elif codigo == 1:
-        calculadora(0.50)
-    elif codigo == 2:
-        calculadora(1)
-    elif codigo == 3:
-        calculadora(4)
-    elif codigo == 5:
-        calculadora(7)
-    elif codigo == 9:
-        calculadora(9)
+    elif codigo in preco:
+        quantidade = int(input("Digite a quantidade comprada: "))
+        custo = quantidade * preco[codigo]
+        soma = soma + custo
     else:
         print ("Codigo inválido")
 
-print ("Você gastará R${:6.2f} comprando essas bagaças aí".format(soma))
+print ("Você gastará R${:5.2f} nesta compra".format(soma))
