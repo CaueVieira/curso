866cd0a77c613f708d2b9bd8d6d32eadfc822f58
Caue Vieira <cauevieira_@hotmail.com>
2018-04-14 10:44:00 -0300

    Learning.

--- Cap 6/Exercício 6.9.py
+++ Cap 6/Exercício 6.9.py
@@ -1,37 +1,36 @@
 """Modifique o exemplo para pesquisar dois valores. Em vez de apenas p, leia outro valor v que também será procurado. Na impressão
 indique qual dos dois valores foi achado primeiro."""
 
-L = [15,7,27,39]
-p = int(input("Digite o primeiro valor a procurar\n"))
-v = int(input("Digite o segundo valor a procurar\n"))
+Lista = [15, 7, 27, 39]
+pesquisa1 = int(input("Digite o primeiro valor a procurar\n"))
+pesquisa2 = int(input("Digite o segundo valor a procurar\n"))
 x = 0
-y = 0
 achouP = False
 achouV = False
-a = 0
-b = 0
+contPesquisa1 = 0
+contPesquisa2 = 0
 
-for numeros in L:
-    if L[x] == p:
+for numeros in Lista:
+    if Lista[x] == pesquisa1:
         achouP = True
-        a = x
-        if v not in L:
-            print("Primeira pesquisa: Valor de {} encontrado na posição {}. \nSegundo valor pesquisado não foi encontrado.".format(p, a))
-    if L[x] == v:
+        contPesquisa1 = x # Trava o contador para depois testar qual vem primeiro.
+        if pesquisa2 not in Lista:
+            print("Primeira pesquisa: Valor de {} encontrado na posição {}. \nSegundo valor pesquisado não foi encontrado.".format(pesquisa1, contPesquisa1))
+    if Lista[x] == pesquisa2:
         achouV = True
-        b = x
-        if p not in L:
-            print("Segunda pesquisa: Valor de {} encontrado na posição {}. \nPrimeiro valor pesquisado não foi encontrado.".format(v, b))
+        contPesquisa2 = x # Trava o contador para depois testar qual vem primeiro.
+        if pesquisa1 not in Lista:
+            print("Segunda pesquisa: Valor de {} encontrado na posição {}. \nPrimeiro valor pesquisado não foi encontrado.".format(pesquisa2, contPesquisa2))
     x += 1
 
 if achouP == False and achouV == False:
     print ("Nenhum dos valores foram encontrados.")
 elif achouP == True and achouV == True:
-    print("Primeira pesquisa: Valor de {} encontrado na posição {}.".format(p, a))
-    print("Segunda pesquisa: Valor de {} encontrado na posição {}.".format(v, b))
-    if a < b:
+    print("Primeira pesquisa: Valor de {} encontrado na posição {}.".format(pesquisa1, contPesquisa1))
+    print("Segunda pesquisa: Valor de {} encontrado na posição {}.".format(pesquisa2, contPesquisa2))
+    if contPesquisa1 < contPesquisa2:
         print ("A primeira pesquisa foi encontrada primeiro.")
-    elif a == b:
+    elif contPesquisa1 == contPesquisa2:
         print ("As duas pesquisas são iguais.")
     else:
         print("A segunda pesquisa encontrada primeiro.")
