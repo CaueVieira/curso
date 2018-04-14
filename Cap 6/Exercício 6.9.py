3319a1709225c5d853e2d6f39975f091fa640566
Caue Vieira <cauevieira_@hotmail.com>
2018-04-14 10:12:22 -0300

    Learning.

---
+++ Cap 6/Exercício 6.9.py
@@ -0,0 +1,37 @@
+"""Modifique o exemplo para pesquisar dois valores. Em vez de apenas p, leia outro valor v que também será procurado. Na impressão
+indique qual dos dois valores foi achado primeiro."""
+
+L = [15,7,27,39]
+p = int(input("Digite o primeiro valor a procurar\n"))
+v = int(input("Digite o segundo valor a procurar\n"))
+x = 0
+y = 0
+achouP = False
+achouV = False
+a = 0
+b = 0
+
+for numeros in L:
+    if L[x] == p:
+        achouP = True
+        a = x
+    if L[x] == v:
+        achouV = True
+        b = x
+    x += 1
+
+if achouP == False and achouV == False:
+    print ("Nenhum dos valores foram encontrados.")
+elif achouP == True and achouV == False:
+    print("Primeira pesquisa: Valor de {} encontrado na posição {}. \nSegundo valor pesquisado não foi encontrado.".format(p, a))
+elif achouP == False and achouV == True:
+    print("Segunda pesquisa: Valor de {} encontrado na posição {}. \nPrimeiro valor não encontrado.".format(v, b))
+else:
+    print("Primeira pesquisa: Valor de {} encontrado na posição {}.".format(p, a))
+    print("Segunda pesquisa: Valor de {} encontrado na posição {}.".format(v, b))
+    if a < b:
+        print ("A primeira pesquisa foi encontrada primeiro.")
+    elif a == b:
+        print ("As duas pesquisas são iguais.")
+    else:
+        print("A segunda pesquisa encontrada primeiro.")
