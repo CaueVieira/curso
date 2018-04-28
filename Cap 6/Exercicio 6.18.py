f4916c763dc90bed2c68937e3addd6bfc1d5a781
Caue Vieira <cauevieira_@hotmail.com>
2018-04-28 13:01:11 -0300

    Learning

---
+++ Cap 6/Exercicio 6.18.py
@@ -0,0 +1,12 @@
+"""Escreva um programa que gere um dicionario, onde cada chave seja um caractere e seu valor seja o número desse caractere encontrando em uma frase lida."""
+import pprint
+
+dicionario = {}
+frase = str(input("Digite uma frase: "))
+
+for caracteres in frase:
+    dicionario.setdefault(caracteres, 1)
+    dicionario[caracteres] += 1
+
+pprint.pprint(dicionario)
+
